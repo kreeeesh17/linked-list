fcf917b6ee790e8a67aee5e1ea66c4b47a4ae1ca
unknown <kreeshmodi.codeforces@gmail.com>
2025-07-05 18:45:01 +0530

    first files uploades

---
+++ LL2Lec1.cpp
@@ -0,0 +1,117 @@
+/**************************************
+05/07
+Author - kreeeesh_17
+Created : 11:06:15
+**************************************/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define f(i,n) for(ll i=0;i<n;i++)
+#define ll long long int
+#define inf 1000000000
+#define endl '\n'
+#define g(i,m,n) for(ll i=m;i<n;i++)
+#define vint vector<ll>
+#define ia(a,n) vint a(n);f(i,n)cin >>a[i];
+#define asc(a) (a).begin(),(a).end()
+#define desc(a) (a).rbegin(),(a).rend()
+#define print(a) for(auto x :(a))cout << x<< ' ';cout<<endl;
+#define pb(x) push_back(x);
+#define pii pair<ll,ll>
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define debarr(a,n) cout<<#a<<" : ";for(int i=0;i<n;i++) cout<<a[i]<<" "; cout<<endl;
+#define debmat(mat,row,col) cout<<#mat<<" :\n";for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cout<<mat[i][j]<<" ";cout<<endl;}
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS)
+template <class S, class T> ostream& operator <<(ostream& os, const unordered_map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T> ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ")";}
+template <class T> ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> ostream& operator <<(ostream& os, const set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> ostream& operator <<(ostream& os, const multiset<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T> ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> void dbs(string str, T t) {cout << str << " : " << t << "\n";}
+template <class T, class... S> void dbs(string str, T t, S... s) {int idx = str.find(','); cout << str.substr(0, idx) << " : " << t << ","; dbs(str.substr(idx + 1), s...);}
+template <class T> void prc(T a, T b) {cout << "["; for (T i = a; i != b; ++i) {if (i != a) cout << ", "; cout << *i;} cout << "]\n";}
+ll mod=1e9+7;
+ll power(ll a, ll b){if(b==0) return 1;if(b%2) return a*power(a,b-1)%mod;else{ll x= power(a,b/2);return (x*x)%mod;}}
+ll inverse(ll x){return power(x,mod-2);}
+ll dx[ ] = { 1 , 0 , -1 , 0 } ;
+ll dy[ ] = { 0 , 1 , 0 , -1 } ;
+/********************************************************************************************************************************************************************************************/
+
+
+struct Node{
+    ll data;
+    Node* next;
+    Node(ll data1, Node* next1){
+        data=data1;
+        next=next1;
+    }
+    Node(ll data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+
+// O(N)
+
+Node* convertarrtoll(vector<ll>&a){
+    Node* head=new Node(a[0]);
+    Node* mover=head;
+    for(ll i=1;i<a.size();i++){
+        Node* temp=new Node(a[i]);
+        mover->next=temp ;
+        mover=temp;
+    }
+    return head;
+}
+
+// O(N)
+
+ll lenofll(Node* head){
+    ll count=0;
+    Node* temp=head;
+    while(temp){
+        temp=temp->next;
+        count++;
+    }
+    return count;
+}
+
+// O(N)
+
+ll isvalpres(Node* head, ll val){
+    Node* temp=head;
+    while(temp){
+        if(temp->data==val) return 1;
+        temp=temp->next;
+    }
+    return 0;
+}
+void solve(){
+    vector<ll>a={2,5,8,7};    
+    Node* head= convertarrtoll(a);
+    cout<<lenofll(head)<<endl;
+    ll val;
+    cin>>val;
+    if(isvalpres(head,val)){
+        cout<<"PRESENT"<<endl;
+    }
+    else{
+        cout<<"NOT PRESENT"<<endl;
+    }
+    Node* temp=head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }    
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    ll t;
+    t=1;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
